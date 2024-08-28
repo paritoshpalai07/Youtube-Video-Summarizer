4493c77e707180d05e77311595c2c176dd786a9c
paritoshpalai07 <148952813+paritoshpalai07@users.noreply.github.com>
2024-08-28 15:13:22 +0530

    Update summarizer.py

--- pages/summarizer.py
+++ pages/summarizer.py
@@ -21,27 +21,27 @@ url = st.text_input("Paste the url of YouTube Video:")
 btn = st.button("Get Notes!")
 
 if btn:
-    # try:
-    vid_id = get_video_id(url)
-    transcript = YouTubeTranscriptApi.get_transcript(vid_id, languages=['en','hi']) 
-    success_msg =  st.success("Hang tight! Your notes are being crafted with the finest virtual quill and ink.")
-
-    summary = ""
-    for line in transcript:
-        summary = summary + line["text"] + " "
-
-    genai.configure(api_key=st.secrets["API_KEY"])
-
-    model = genai.GenerativeModel('gemini-1.5-flash')
-
-    prompt = f"X is the transcript of the youtube video. X : {summary}. I want to make notes of each topic in transcript and make all possible Q&A on each with answers"
-
-    response = model.generate_content(prompt)
-
-    response = response.text.replace("Transcript", "Video")
-    response = response.replace("transcript", "video")
-    if success_msg:
-        success_msg.empty()
-    st.write(response)
-    # except:
-    #     st.warning("Sorry! Video Notes for this video cannot be generated")
+    try:
+        vid_id = get_video_id(url)
+        transcript = YouTubeTranscriptApi.get_transcript(vid_id, languages=['en','hi']) 
+        success_msg =  st.success("Hang tight! Your notes are being crafted with the finest virtual quill and ink.")
+    
+        summary = ""
+        for line in transcript:
+            summary = summary + line["text"] + " "
+    
+        genai.configure(api_key=st.secrets["API_KEY"])
+    
+        model = genai.GenerativeModel('gemini-1.5-flash')
+    
+        prompt = f"X is the transcript of the youtube video. X : {summary}. I want to make notes of each topic in transcript and make all possible Q&A on each with answers"
+    
+        response = model.generate_content(prompt)
+    
+        response = response.text.replace("Transcript", "Video")
+        response = response.replace("transcript", "video")
+        if success_msg:
+            success_msg.empty()
+        st.write(response)
+    except:
+        st.warning("Sorry! Video Notes for this video cannot be generated")
